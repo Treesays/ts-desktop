1c3ac8dbd6a114f007152377b9e2d3d128710539
n8659150 <jackli900421@hotmail.com>
2019-12-15 22:28:43 +0800

    各板块对接，无用代码清理

--- src/views/GeekCode/GeekCode.vue
+++ src/views/GeekCode/GeekCode.vue
@@ -1,213 +1,223 @@
 <template>
-  <div class="hc-container">
-    <div class="hc-posts">
-      <el-tabs v-model="activeName" @tab-click="handleClick">
-        <el-tab-pane label="热门" name="all">
-          <div v-for="(post, index) in hotPosts" :key="index" class="hc-post-layout">
-            <div class="hc-post-item">
-              <div class="user-info">
-                <div class="user-avatar">
-                  <el-avatar
-                    src="https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png"
-                  ></el-avatar>
-                </div>
-                <div class="author-info">
-                  <h3>{{post.username}}</h3>
-                  <span>{{post.position}} @</span>
-                  <span>{{post.workplace}}</span>
-                </div>
-              </div>
-              <div class="post-content">
-                <span>{{post.content}}</span>
-              </div>
-              <div class="post-tags">
-                <el-tag
-                  v-for="(tag, index) in post.tags"
-                  :key="index"
-                  size="small"
-                  effect="plain"
-                  class="post-tag"
-                >{{ tag }}</el-tag>
-              </div>
-              <div class="post-stats">
-                <span>赞</span>
-                <el-divider class="post-stats-divider" direction="vertical"></el-divider>
-                <span @click="loadComments()">评论</span>
-                <el-divider class="post-stats-divider" direction="vertical"></el-divider>
-                <span>分享</span>
-              </div>
-            </div>
-          </div>
-        </el-tab-pane>
-        <el-tab-pane label="最新" name="inAuth">
-          <div v-for="(post, index) in newPosts" :key="index" class="hc-post-layout">
-            <div class="hc-post-item">
-              <div class="user-info">
-                <div class="user-avatar">
-                  <el-avatar
-                    src="https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png"
-                  ></el-avatar>
-                </div>
-                <div class="author-info">
-                  <h3>{{post.username}}</h3>
-                  <span>{{post.position}} @</span>
-                  <span>{{post.workplace}}</span>
-                </div>
-              </div>
-              <div class="post-content">
-                <span>{{post.content}}</span>
-              </div>
-              <div class="post-tags">
-                 <el-tag
-                  v-for="(tag, index) in post.tags"
-                  :key="index"
-                  size="small"
-                  effect="plain"
-                  class="post-tag"
-                >{{ tag }}</el-tag>
-              </div>
-              <div class="post-stats">
-                <span>赞</span>
-                <el-divider class="post-stats-divider" direction="vertical"></el-divider>
-                <span @click="loadComments()">评论</span>
-                <el-divider class="post-stats-divider" direction="vertical"></el-divider>
-                <span>分享</span>
-              </div>
-            </div>
-          </div>
-        </el-tab-pane>
-      </el-tabs>
-    </div>
+    <div class="hc-container">
+        <div class="hc-posts">
+            <el-tabs v-model="activeName" @tab-click="handleClick">
+                <el-tab-pane label="热门" name="all">
+                    <div v-for="(post, index) in hotPosts" :key="index" class="hc-post-layout">
+                        <div class="hc-post-item">
+                            <div class="user-info">
+                                <div class="user-avatar">
+                                    <el-avatar src="https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png"></el-avatar>
+                                </div>
+                                <div class="author-info">
+                                    <h3>{{post.username}}</h3>
+                                    <span>{{post.position}} @</span>
+                                    <span>{{post.workplace}}</span>
+                                </div>
+                            </div>
+                            <div class="post-content">
+                                <span>{{post.content}}</span>
+                            </div>
+                            <div class="post-tags">
+                                <el-tag v-for="(tag, index) in post.tags" :key="index" size="small" effect="plain" class="post-tag">{{ tag }}</el-tag>
+                            </div>
+                            <div class="post-stats">
+                                <span>赞</span>
+                                <el-divider class="post-stats-divider" direction="vertical"></el-divider>
+                                <span @click="loadComments()">评论</span>
+                                <el-divider class="post-stats-divider" direction="vertical"></el-divider>
+                                <span>分享</span>
+                            </div>
+                        </div>
+                    </div>
+                </el-tab-pane>
+                <el-tab-pane label="最新" name="inAuth">
+                    <div v-for="(post, index) in newPosts" :key="index" class="hc-post-layout">
+                        <div class="hc-post-item">
+                            <div class="user-info">
+                                <div class="user-avatar">
+                                    <el-avatar src="https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png"></el-avatar>
+                                </div>
+                                <div class="author-info">
+                                    <h3>{{post.username}}</h3>
+                                    <span>{{post.position}} @</span>
+                                    <span>{{post.workplace}}</span>
+                                </div>
+                            </div>
+                            <div class="post-content">
+                                <span>{{post.content}}</span>
+                            </div>
+                            <div class="post-tags">
+                                <el-tag v-for="(tag, index) in post.tags" :key="index" size="small" effect="plain" class="post-tag">{{ tag }}</el-tag>
+                            </div>
+                            <div class="post-stats">
+                                <span>赞</span>
+                                <el-divider class="post-stats-divider" direction="vertical"></el-divider>
+                                <span @click="loadComments()">评论</span>
+                                <el-divider class="post-stats-divider" direction="vertical"></el-divider>
+                                <span>分享</span>
+                            </div>
+                        </div>
+                    </div>
+                </el-tab-pane>
+            </el-tabs>
+        </div>
 
-    <div class="hc-form">
-      <el-card class="box-card" shadow="never">
-        <div class="topic-wallpaper">
-          <span class="wallpaper"></span>
+        <div class="hc-form">
+            <el-card class="box-card" shadow="never">
+                <div class="topic-wallpaper">
+                    <span class="wallpaper"></span>
+                </div>
+                <div class="topic-intro">
+                    <el-avatar class="topic-avatar" shape="square" :size="50"></el-avatar>
+                    <div class="topic-intro-main">
+                        <span class="topic-title">{{ topicInfo.name }}</span>
+                        <el-button type="success" size="small" @click="followUnfollow()">{{ isUserFollowedThisCategory ? '已关注' : '关 注'}}</el-button>
+                    </div>
+                    <div class="topic-description">
+                        <div style="font-weight: bold;">话题介绍:</div>
+                        <span>show出代码，show出自己，不计语言，不计行数。</span>
+                    </div>
+                </div>
+                <div class="topic-stats">
+                    <span>
+                        <div class="count">{{ postsCount }}</div>
+                        <div class="item">沸点</div>
+                    </span>
+                    <el-divider class="post-stats-divider" direction="vertical"></el-divider>
+                    <span>
+                        <div class="count">{{ followersCount }}</div>
+                        <div class="item">关注</div>
+                    </span>
+                </div>
+            </el-card>
         </div>
-        <div class="topic-intro">
-          <el-avatar class="topic-avatar" shape="square" :size="50"></el-avatar>
-          <div class="topic-intro-main">
-            <span class="topic-title">{{ topicInfo.name }}</span>
-            <el-button
-              type="success"
-              size="small"
-              @click="followUnfollow()"
-            >{{ isUserFollowedThisCategory ? '已关注' : '关 注'}}</el-button>
-          </div>
-          <div class="topic-description">
-            <div style="font-weight: bold;">话题介绍:</div>
-            <span>show出代码，show出自己，不计语言，不计行数。</span>
-          </div>
-        </div>
-        <div class="topic-stats">
-          <span>
-            <div class="count">{{ postsCount }}</div>
-            <div class="item">沸点</div>
-          </span>
-          <el-divider class="post-stats-divider" direction="vertical"></el-divider>
-          <span>
-            <div class="count">{{ followersCount }}</div>
-            <div class="item">关注</div>
-          </span>
-        </div>
-      </el-card>
     </div>
-  </div>
 </template>
 
 <script>
 import "@/resources/overwrite.css";
 import "@/views/styles/views-main.css";
 import {
-  fetchCategoryStats,
-  fetchPostsByCategory,
-  followUnfollow,
-  isUserFollowedCategory
+    fetchCategoryStats,
+    fetchPostsByCategory,
+    followUnfollow,
+    isUserFollowedCategory
 } from "@/services/categoryManipulate.js";
 export default {
-  name: "Treesays",
-  data() {
-    return {
-      posts: null,
-      followersCount: 0,
-      postsCount: 0,
-      isUserFollowedThisCategory: false,
-      activeName: "all",
-      labelPosition1: "top",
-      labelPosition2: "left",
-      topicInfo: {
-        name: "代码秀"
-      },
-      squareUrl:
-        "https://mirror-gold-cdn.xitu.io/168e08de7600547699f?imageView2/1/w/100/h/100/q/85/format/webp/interlace/1",
-    };
-  },
-  computed: {
-    hotPosts() {
-        return this.posts ? this.posts.filter(post => post.tags.length === 1) : []; //后续更改
+    name: "Treesays",
+    data() {
+        return {
+            posts: null,
+            followersCount: 0,
+            postsCount: 0,
+            isUserFollowedThisCategory: false,
+            activeName: "all",
+            labelPosition1: "top",
+            labelPosition2: "left",
+            topicInfo: {
+                name: "代码秀"
+            },
+            squareUrl:
+                "https://mirror-gold-cdn.xitu.io/168e08de7600547699f?imageView2/1/w/100/h/100/q/85/format/webp/interlace/1"
+        };
     },
-    newPosts() {
-        return this.posts ? this.posts.filter(post => post.tags.length !== 1) : []; //后续更改
+    computed: {
+        hotPosts() {
+            return this.posts
+                ? this.posts.filter(post => post.tags.length === 1)
+                : []; //后续更改
+        },
+        newPosts() {
+            return this.posts
+                ? this.posts.filter(post => post.tags.length !== 1)
+                : []; //后续更改
+        },
+        currentUserId() {
+            return AV.User.current() ? AV.User.current()["id"] : null;
+        }
     },
-    currentUserId() {
-      return AV.User.current() ? AV.User.current()["id"] : null;
+    methods: {
+        handleClick(tab, event) {
+            console.log(tab, event);
+        },
+        loadComments() {
+            // 先检查是否登录。
+            if (!this.currentUserId) {
+                this.$store.dispatch("showLogin", true);
+            }
+        },
+        async followUnfollow() {
+            if (!this.currentUserId) {
+                this.$store.dispatch("showLogin", true);
+            } else {
+                const response = await followUnfollow(
+                    "GeekCode",
+                    this.currentUserId
+                );
+                this.isUserFollowedThisCategory = response["index"] === -1;
+            }
+        },
+        async categoryStatsInit() {
+            const {
+                attributes: { followers, posts }
+            } = await fetchCategoryStats("GeekCode");
+            this.followersCount = followers.length;
+            this.postsCount = posts.length;
+            const _posts = await fetchPostsByCategory("GeekCode");
+            if (_posts) {
+                this.posts = _posts.map(post => {
+                    const {
+                        _serverData: {
+                            category,
+                            content,
+                            username,
+                            position,
+                            workplace,
+                            avatar,
+                            tags,
+                            upCount,
+                            shareCount
+                        }
+                    } = post;
+                    return {
+                        category,
+                        content,
+                        username,
+                        position,
+                        workplace,
+                        avatar,
+                        tags,
+                        upCount,
+                        shareCount
+                    };
+                });
+            }
+        }
+    },
+    async mounted() {
+        this.categoryStatsInit();
+        if (this.currentUserId) {
+            this.isUserFollowedThisCategory = await isUserFollowedCategory(
+                "GeekCode",
+                this.currentUserId
+            );
+        }
     }
-  },
-  methods: {
-    handleClick(tab, event) {
-      console.log(tab, event);
-    },
-    loadComments() {
-      // 先检查是否登录。
-      if (!this.currentUserId) {
-        this.$store.dispatch("showLogin", true);
-      }
-    },
-    async followUnfollow() {
-      if (!this.currentUserId) {
-        this.$store.dispatch("showLogin", true);
-      } else {
-        const response = await followUnfollow("GeekCode", this.currentUserId);
-        this.isUserFollowedThisCategory = response["index"] === -1;
-      }
-    },
-    async categoryStatsInit() {
-      const {
-        attributes: { followers, posts }
-      } = await fetchCategoryStats("GeekCode");
-      this.followersCount = followers.length;
-      this.postsCount = posts.length;
-      const _posts = await fetchPostsByCategory("GeekCode");
-      if (_posts) { 
-            this.posts = _posts.map((post) => {
-            const { _serverData: { category, content, username, position, workplace, avatar, tags, upCount, shareCount } } = post;
-            return { category, content, username, position, workplace, avatar, tags, upCount, shareCount }
-      })}
-    }
-  },
-  async mounted() {
-    this.categoryStatsInit();
-    if (this.currentUserId) {
-      this.isUserFollowedThisCategory = await isUserFollowedCategory(
-        "GeekCode",
-        this.currentUserId
-      );
-    }
-  }
 };
 </script>
 
 <!-- Add 'scoped' attribute to limit CSS to this component only -->
 <style scoped>
 .topic-wallpaper .wallpaper {
-  background-image: url("~@/assets/treesays_code.png");
-  background-repeat: no-repeat;
-  background-position: 50%;
-  background-size: 120%;
+    background-image: url("~@/assets/treesays_code.png");
+    background-repeat: no-repeat;
+    background-position: 50%;
+    background-size: 120%;
 }
 .topic-intro .topic-avatar {
-  background-image: url("~@/assets/treesays_code.png");
-  background-size: cover;
-  background-position-x: 50%;
+    background-image: url("~@/assets/treesays_code.png");
+    background-size: cover;
+    background-position-x: 50%;
 }
 </style>
